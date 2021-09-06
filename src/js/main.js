0f1011208d820177a5c36e3e62398d137dbd710d
dima doronin <warreaver123@gmail.com>
2021-09-06 23:18:49 +0300

    forms

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,18 @@ import MainSlider from "./modules/sliders/main-slider";
 import PlayVideo from './modules/playVideo';
 import SliderMini from './modules/sliders/slider-mini'
 import Card from "./modules/card";
+import Form from './modules/form';
 
 window.addEventListener('DOMContentLoaded', () => {
-
+    //MAIN SLIDER
     const slider = new MainSlider({ container:'.page',  btns:'.next'});
     slider.render();
 
+    //MAIN VIDEO
     const video = new PlayVideo('.showup .play', '.overlay');
     video.init();
 
+    // MINI SLIDERS
     const sliderShowup = new SliderMini({
         container: '.showup__content-slider',
         prev: '.showup__prev', 
@@ -37,9 +40,18 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     feedSlider.init();
 
+    //CARD
     const cardOfficerOld = new Card('.officerold', '.officerold .plus');
     cardOfficerOld.init();
 
     const cardOfficerNew = new Card('.officernew', '.officernew .plus');
     cardOfficerNew.init();
+
+    //FORM
+
+   const mainForm = new Form('.join__wrapper form', 'input', '[data-email]');
+   mainForm.init();
+
+   const sheduleForm = new Form('.schedule__form form', 'input', '[data-email]');
+   sheduleForm.init();
 })
---
+++ src/js/modules/form.js
@@ -0,0 +1,140 @@
+export default class Form {
+    constructor(form, input, emailInput) {
+        this.form = document.querySelector(form);
+        this.inputs = this.form.querySelectorAll(input);
+        this.eInput = this.form.querySelectorAll(emailInput);
+    }
+
+    async postData(url, data) {
+        const res = await fetch(url, {
+            method:'POST',
+            body: data
+        })
+
+        return await res.text();
+    }
+
+    clearInputs() {
+        this.inputs.forEach(input => {
+            input.value = '';
+        });
+    }
+
+    validate() {
+        this.eInput.forEach(input => {
+            input.addEventListener('keypress', (e) => {
+                if (e.key.match(/[^a-z @ \.]/ig)) {
+                    e.preventDefault();
+                }
+            })
+          
+        })
+    }
+
+    mask() {
+        let setCursorPosition = (pos, elem) => {
+            elem.focus();
+            
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                let range = elem.createTextRange();
+    
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
+    
+        function createMask(event) {
+            let matrix = '+1 (___) ___-__',
+                i = 0,
+                def = matrix.replace(/\D/g, ''),
+                val = this.value.replace(/\D/g, '');
+    
+            if (def.length >= val.length) {
+                val = def;
+            }
+    
+            this.value = matrix.replace(/./g, function(a) {
+                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+            });
+    
+            if (event.type === 'blur') {
+                if (this.value.length == 2) {
+                    this.value = '';
+                }
+            } else {
+                setCursorPosition(this.value.length, this);
+            }
+        }
+    
+        let inputs = document.querySelectorAll('[name="phone"]');
+    
+        inputs.forEach(input => {
+            input.addEventListener('input', createMask);
+            input.addEventListener('focus', createMask);
+            input.addEventListener('blur', createMask);
+        });
+    }
+
+    main(message) {
+        this.form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let messageBlock = document.createElement('div');
+            messageBlock.classList.add('animated', 'fadeInUp');
+            messageBlock.textContent = message.loading;
+            // messageBlock.style.fontSize = '20px';
+            messageBlock.style.cssText = `
+            display: flex;
+            padding: 50px 0;
+            font-size: 50px;
+            color: #ccc;
+            text-align: center;
+            justify-content: center;
+            `;
+            this.form.parentNode.appendChild(messageBlock);
+
+            this.form.classList.add('animated', 'fadeInOut');
+            setTimeout(() => {
+                this.form.style.display = 'none';
+            }, 400);
+
+
+            const formData = new FormData(this.form);
+
+            this.postData('assets/question.php', formData)
+                .then(res => {
+                    console.log(res);
+                    messageBlock.textContent = message.accept;
+                })
+                .catch(() => {
+                    console.log('что-то не так');
+                    messageBlock.textContent = message.fail;
+                })
+                .finally(() => {
+                    this.clearInputs();
+                    setTimeout(() => {
+                        messageBlock.remove();
+                        this.form.style.display = 'block';
+                        this.form.classList.remove('fadeInOut');
+                        this.form.classList.add('fadeInUp');
+                    }, 4000)
+                }) 
+        })
+    }
+
+    init() {
+
+       let message = {
+            accept: 'Спасибо! Мы с вами свяжемся',
+            fail: 'Что-то пошло не так',
+            loading: 'Загрузка'
+        }
+        this.validate();
+        this.main(message);
+        this.mask();
+    }
+}
